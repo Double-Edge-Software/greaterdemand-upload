8492f635fc9edd045b37769a10bb95c18c1e1c2b
carter3689 <carter3689@gmail.com>
2020-02-18 11:43:37 -0600

    Added Date to Database Object

--- src/app/upload-task/upload-task.component.ts
+++ src/app/upload-task/upload-task.component.ts
@@ -51,7 +51,7 @@ export class UploadTaskComponent implements OnInit {
       finalize( async() =>  {
         this.downloadURL = await ref.getDownloadURL().toPromise();
 
-        this.db.collection('files').add( {Name: this.customerName, TaxYear:this.taxYear, downloadURL: this.downloadURL, path });
+        this.db.collection('files').add( {Name: this.customerName, TaxYear:this.taxYear, downloadURL: this.downloadURL, path, UploadDate: new Date().toLocaleString()  });
       }),
     );
   }
